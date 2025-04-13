deed5e66a236bc72703b8a0309d2f9b6836a7321
lujo <58606086+LudoJoris@users.noreply.github.com>
2025-04-13 08:28:09 +0200

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -1,9 +1,6 @@
 // https://ludojoris.github.io/microbit-ks4036f/
 
 let i2c_addr = 0x30;
-let left_bias = 0;
-let right_bias = 0;
-let speed = 0;
 
 enum LR {
   //% block="links"
@@ -19,35 +16,12 @@ enum LRB {
   //% block="beide"
   B = 2,
 }
-
-//enum Motor {
-  //% block="links"
-  //M0 = 0,
-  //% block="rechts"
-  //M1 = 1,
-  //% block="beide"
-  //M2 = 2,
-//}
 enum Richting {
   //% block="vooruit"
   D0 = 0,
   //% block="achteruit"
   D1 = 1,
 }
-//enum Led {
-  //% block="links"
-  //L0 = 0,
-  //% block="rechts"
-  //L1 = 1,
-  //% block="beide"
-  //L2 = 2,
-//}
-//enum Bias {
-  //% block="links"
-  //B0 = 0,
-  //% block="rechts"
-  //B1 = 1,
-//}
 enum RGB {
   //% block=rood
   Red = 0x7F0000,
@@ -70,216 +44,222 @@ enum RGB {
 //% color="#AA278D"
 namespace SmartCar {
 
-    //% group="Motor" weight=90 blockGap=4
-    //% block="motor $motor richting $richting snelheid $snelheid"
-    //% snelheid.min=0 snelheid.max=255 snelheid.defl=100
-    export function motor(motor: LRB, richting: Richting, snelheid: number) {
-      if (motor == 0) {
-        if (richting == 0) {
-          i2c_w(0x01, 0);
-          i2c_w(0x02, snelheid + left_bias);
-        }
-        if (richting == 1) {
-          i2c_w(0x01, snelheid + left_bias);
-          i2c_w(0x02, 0);
-        }
-      }
-      if (motor == 1) {
-        if (richting == 0) {
-          i2c_w(0x03, snelheid + right_bias);
-          i2c_w(0x04, 0);
-        }
-        if (richting == 1) {
-          i2c_w(0x03, 0);
-          i2c_w(0x04, snelheid + right_bias);
-        }
-      }
-      if (motor == 2) {
-        if (richting == 0) {
-          i2c_w(0x01, 0);
-          i2c_w(0x02, snelheid + left_bias);
-          i2c_w(0x03, snelheid + left_bias);
-          i2c_w(0x04, 0);
-        }
-        if (richting == 1) {
-          i2c_w(0x01, snelheid + right_bias);
-          i2c_w(0x02, 0);
-          i2c_w(0x03, 0);
-          i2c_w(0x04, snelheid + right_bias);
-        }
-      }
-    }
+  let left_bias = 0;
+  let right_bias = 0;
+  let left_speed = 0;
+  let right_speed = 0;
 
-    //% group="Motor" weight=85  blockGap=4
-    //% block="motor $motor stop"
-    export function stop(motor: LRB) {
-      if (motor == 0) {
+  //% block="motor $motor richting $richting snelheid $snelheid"
+  //% group="Motor" weight=90 blockGap=4
+  //% snelheid.min=0 snelheid.max=255
+  export function motor(motor: LRB, richting: Richting, snelheid: number) {
+    if (motor == 0) {
+      if (richting == 0) {
         i2c_w(0x01, 0);
+        i2c_w(0x02, snelheid + left_bias);
+      }
+      if (richting == 1) {
+        i2c_w(0x01, snelheid + left_bias);
         i2c_w(0x02, 0);
       }
-      if (motor == 1) {
-        i2c_w(0x03, 0);
+    }
+    if (motor == 1) {
+      if (richting == 0) {
+        i2c_w(0x03, snelheid + right_bias);
         i2c_w(0x04, 0);
       }
-      if (motor == 2) {
+      if (richting == 1) {
+        i2c_w(0x03, 0);
+        i2c_w(0x04, snelheid + right_bias);
+      }
+    }
+    if (motor == 2) {
+      if (richting == 0) {
         i2c_w(0x01, 0);
+        i2c_w(0x02, snelheid + left_bias);
+        i2c_w(0x03, snelheid + left_bias);
+        i2c_w(0x04, 0);
+      }
+      if (richting == 1) {
+        i2c_w(0x01, snelheid + right_bias);
         i2c_w(0x02, 0);
         i2c_w(0x03, 0);
-        i2c_w(0x04, 0);
+        i2c_w(0x04, snelheid + right_bias);
       }
     }
+  }
 
-    //% block="snelheid"
-    //% group="Motor"
-    export function get_speed(): number {
-      return speed;
+  //% block="motor $motor stop"
+  //% group="Motor" weight=85  blockGap=4
+  export function stop(motor: LRB) {
+    if (motor == 0) {
+      i2c_w(0x01, 0);
+      i2c_w(0x02, 0);
     }
+    if (motor == 1) {
+      i2c_w(0x03, 0);
+      i2c_w(0x04, 0);
+    }
+    if (motor == 2) {
+      i2c_w(0x01, 0);
+      i2c_w(0x02, 0);
+      i2c_w(0x03, 0);
+      i2c_w(0x04, 0);
+    }
+  }
+
+  //% block="snelheid $motor"
+  //% group="Motor"
+  export function get_speed(motor: LR): number {
+    if (motor == 0) {
+      return left_speed;
+    }
+    if (motor == 1) {
+      return right_speed;
+    }
+    return 0;
+  }
   
-    /**
-     * pas motor snelheid aan, blijft van kracht tot herstart
-     * kies voor de zwakste motor
-     */
-    //% group="Motor" weight=80 blockGap=4
-    //% block="motor %motor plus %bias"
-    //% bias.min=0 bias.max=30
-    export function set_bias(motor: LR, bias: number): void {
-      if (motor == 0) {
-        left_bias = Math.floor((100 + bias) / 100);
-        right_bias = 0;
-      }
-      if (motor == 1) {
-        left_bias = 0;
-        right_bias = Math.floor((100 + bias) / 100);
-      }
+  //% block="motor $motor plus $bias"
+  //% group="Motor" weight=80 blockGap=4
+  //% bias.min=0 bias.max=50
+  export function set_bias(motor: LR, bias: number): void {
+    left_bias = 0;
+    right_bias = 0;
+    if (motor == 0) {
+      left_bias = Math.floor((100 + bias) / 100);
     }
-    //% block="motor bias $motor"
-    //% group="Motor"
-    export function get_bias(motor: LR): number {
-      let bias = 0;
-      if (motor == 0) {
-        bias = left_bias;
-      }
-      if (motor == 1) {
-        bias = right_bias;
-      }
-      return bias;
+    if (motor == 1) {
+      right_bias = Math.floor((100 + bias) / 100);
     }
-  
+  }
+
+  //% block="motor bias $motor"
+  //% group="Motor"
+  export function get_bias(motor: LR): number {
+    if (motor == 0) {
+      return left_bias;
+    }
+    if (motor == 1) {
+      return right_bias;
+    }
+    return 0;
+  }
 
   
-    //% group="LED" weight=70 blockGap=4
-    //% block="LED $led met kleur $rgb" 
-    export function set_led(led: LR, rgb: number) {
+  //% block="LED $led met kleur $rgb" 
+  //% group="LED" weight=70 blockGap=4
+  export function set_led(led: LR, rgb: number) {
     
-      let r = 255 - unpackR(rgb);
-      let g = 255 - unpackG(rgb);
-      let b = 255 - unpackB(rgb);
+    let r = 255 - unpackR(rgb);
+    let g = 255 - unpackG(rgb);
+    let b = 255 - unpackB(rgb);
 
-      switch (led) {
-        case 0:
-          i2c_w(0x08, r);
-          i2c_w(0x07, g);
-          i2c_w(0x06, b);
-          break;
-        case 1:
-          i2c_w(0x09, r);
-          i2c_w(0x0a, g);
-          i2c_w(0x05, b);
-          break;
-        case 2:
-          i2c_w(0x08, r);
-          i2c_w(0x07, g);
-          i2c_w(0x06, b);
-          i2c_w(0x09, r);
-          i2c_w(0x0a, g);
-          i2c_w(0x05, b);
-          break;
-      }
+    switch (led) {
+      case 0:
+        i2c_w(0x08, r);
+        i2c_w(0x07, g);
+        i2c_w(0x06, b);
+        break;
+      case 1:
+        i2c_w(0x09, r);
+        i2c_w(0x0a, g);
+        i2c_w(0x05, b);
+        break;
+      case 2:
+        i2c_w(0x08, r);
+        i2c_w(0x07, g);
+        i2c_w(0x06, b);
+        i2c_w(0x09, r);
+        i2c_w(0x0a, g);
+        i2c_w(0x05, b);
+        break;
     }
+  }
 
-    //% group="LED" weight=60 blockGap=8
-    //% block="LED $led uit" 
-    export function reset_led(led: LR) {
-      switch (led) {
-        case 0:
-          i2c_w(0x08, 255);
-          i2c_w(0x07, 255);
-          i2c_w(0x06, 255);
-          break;
-        case 1:
-          i2c_w(0x09, 255);
-          i2c_w(0x0a, 255);
-          i2c_w(0x05, 255);
-          break;
-        case 2:
-          i2c_w(0x08, 255);
-          i2c_w(0x07, 255);
-          i2c_w(0x06, 255);
-          i2c_w(0x09, 255);
-          i2c_w(0x0a, 255);
-          i2c_w(0x05, 255);
-          break;
-      }
+  //% block="LED $led uit" 
+  //% group="LED" weight=60 blockGap=8
+  export function reset_led(led: LR) {
+    switch (led) {
+      case 0:
+        i2c_w(0x08, 255);
+        i2c_w(0x07, 255);
+        i2c_w(0x06, 255);
+        break;
+      case 1:
+        i2c_w(0x09, 255);
+        i2c_w(0x0a, 255);
+        i2c_w(0x05, 255);
+        break;
+      case 2:
+        i2c_w(0x08, 255);
+        i2c_w(0x07, 255);
+        i2c_w(0x06, 255);
+        i2c_w(0x09, 255);
+        i2c_w(0x0a, 255);
+        i2c_w(0x05, 255);
+        break;
     }
+  }
     
-    //% group="LED" weight=50 blockGap=8
-    //% block="rood $red groen $green blauw $blue"
-    //% rood.min=0 rood.max=255 rood.defl=0
-    //% groen.min=0 groen.max=255 groen.defl=0
-    //% blauw.min=0 blauw.max=255 blauw.defl=0
-    export function rgb(red: number, green: number, blue: number): number {
-      return packRGB(red, green, blue);
-    }
+  //% block="rood $red groen $green blauw $blue"
+  //% group="LED" weight=50 blockGap=8
+  //% rood.min=0 rood.max=255
+  //% groen.min=0 groen.max=255
+  //% blauw.min=0 blauw.max=255
+  export function rgb(red: number, green: number, blue: number): number {
+    return packRGB(red, green, blue);
+  }
 
-    //% group="LED" weight=40 blockGap=8
-    //% block="%color"
-    export function colors(color: RGB): number {
-      return color;
-    }
-
-
-    pins.setPull(DigitalPin.P14, PinPullMode.PullNone);
-    //% group="Ultrasone sensor" weight=70 blockGap=4
-    //% block="afstand (cm)"
-    export function ping(): number {
-
-      pins.digitalWritePin(DigitalPin.P14, 0);
-      control.waitMicros(2);
-      pins.digitalWritePin(DigitalPin.P14, 1);
-      control.waitMicros(10);
-      pins.digitalWritePin(DigitalPin.P14, 0);
- 
-      let d = pins.pulseIn(DigitalPin.P15, PulseValue.High, 500 * 58); // max 500 cm
-      return Math.floor(d / 58);  // cm
-    }
+  //% block="$color"
+  //% group="LED" weight=40 blockGap=8
+  export function colors(color: RGB): number {
+    return color;
+  }
 
 
-    pins.setPull(DigitalPin.P12, PinPullMode.PullUp);
-    pins.setPull(DigitalPin.P13, PinPullMode.PullUp);
-    //% group="Infrarood sensor" weight=80 blockGap=6
-    //% block="IR sensor links"
-    export function ir_sensor_links(): boolean {
-      return pins.digitalReadPin(DigitalPin.P13) == 1;
-    }
+  pins.setPull(DigitalPin.P14, PinPullMode.PullNone);
+  //% block="afstand (cm)"
+  //% group="Ultrasone sensor" weight=70 blockGap=4
+  export function ping(): number {
 
-    //% group="Infrarood sensor" weight=70 blockGap=6
-    //% block="IR sensor rechts"
-    export function ir_sensor_rechts(): boolean {
-      return pins.digitalReadPin(DigitalPin.P12) == 1;
-    }
+    pins.digitalWritePin(DigitalPin.P14, 0);
+    control.waitMicros(2);
+    pins.digitalWritePin(DigitalPin.P14, 1);
+    control.waitMicros(10);
+    pins.digitalWritePin(DigitalPin.P14, 0);
+
+    let d = pins.pulseIn(DigitalPin.P15, PulseValue.High, 500 * 58); // max 500 cm
+    return Math.floor(d / 58);  // cm
+  }
 
 
-    //% group="Lichtgevoelige weerstand" weight=60 blockGap=6
-    //% block="LDR links"
-    export function ldr_links(): number {
-      return pins.analogReadPin(AnalogPin.P1);
-    }
+  pins.setPull(DigitalPin.P12, PinPullMode.PullUp);
+  pins.setPull(DigitalPin.P13, PinPullMode.PullUp);
+  //% block="IR sensor links"
+  //% group="Infrarood sensor" weight=80 blockGap=6
+  export function ir_sensor_links(): boolean {
+    return pins.digitalReadPin(DigitalPin.P13) == 1;
+  }
 
-    //% group="Lichtgevoelige weerstand" weight=50 blockGap=6
-    //% block="LDR rechts"
-    export function ldr_rechts(): number {
-      return pins.analogReadPin(AnalogPin.P0);
-    }
+  //% block="IR sensor rechts"
+  //% group="Infrarood sensor" weight=70 blockGap=6
+  export function ir_sensor_rechts(): boolean {
+    return pins.digitalReadPin(DigitalPin.P12) == 1;
+  }
+
+
+  //% block="LDR links"
+  //% group="Lichtgevoelige weerstand" weight=60 blockGap=6
+  export function ldr_links(): number {
+    return pins.analogReadPin(AnalogPin.P1);
+  }
+
+  //% block="LDR rechts"
+  //% group="Lichtgevoelige weerstand" weight=50 blockGap=6
+  export function ldr_rechts(): number {
+    return pins.analogReadPin(AnalogPin.P0);
+  }
 
 }
 
@@ -297,9 +277,8 @@ function unpackB(rgb: number): number {
 }
 
 function i2c_w(reg: number, value: number) {
-    let buf = pins.createBuffer(2)
-    buf[0] = reg
-    buf[1] = value
-    pins.i2cWriteBuffer(i2c_addr, buf)
+  let buf = pins.createBuffer(2)
+  buf[0] = reg
+  buf[1] = value
+  pins.i2cWriteBuffer(i2c_addr, buf)
 }
-
